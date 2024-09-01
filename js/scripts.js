7c39b8ee1886512fb0e1f901767a515059da0756
claudiopatti <claudiopatti1997@gmail.com>
2024-09-01 18:08:23 +0200

    in fase di aggiustamento grafica

--- js/scripts.js
+++ js/scripts.js
@@ -25,11 +25,11 @@ const { createApp } = Vue
         },
         methods: {
             removeMission(i) {
-                this.missions.splice(i, 1);
+                this.missions.splice(i, 1); // splice ti rimuove un elemento a partire dal primo numero che si da e il secondo elemento e per specificare quanti sono da rimuovere
 
             },
             addMission() {
-                const clearNewText = this.newtext.trim();
+                const clearNewText = this.newtext.trim(); // serve ad rimuovere spazi prima e dopo le parole o frasi da inserire
 
                 if (clearNewText != '') {
                     this.missions.push({text : clearNewText ,  done : this.newDone})
@@ -45,7 +45,7 @@ const { createApp } = Vue
                 else {
                     this.missions[i].done = true 
                 }
-                console.log(this.missions)
+                // console.log(this.missions)
             }
         }
     }).mount('#app')
